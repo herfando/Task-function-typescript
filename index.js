5d6c3e8dc74d1255d3ccad47cc70a4bf7ceee315
Herfando <herfando_91@yahoo.com>
2025-08-20 07:13:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function tambah(a, b) {
+    return a + b;
+}
+console.log(tambah(10, 20)); // Output: 12
---
+++ index.ts
@@ -0,0 +1,14 @@
+function tambah(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(tambah(5, 5)); // Output: 12
+
+function sapa(nama: string): void {
+  console.log(`Halo, ${nama} !`);
+}
+
+// Tes
+sapa("Budi");   // Output: Halo, Budi!
+sapa("Ani");    // Output: Halo, Ani!
+
